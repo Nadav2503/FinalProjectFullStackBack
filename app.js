47dafa77f004fd184856a4252fd1aabfd9914a7c
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 09:54:26 +0300

    apply the midllewares logger and error handling in app

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-// Import required modules
-const express = require("express");  // Web framework for Node.js
-require("dotenv").config();           // Load environment variables from .env file
-const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import the database connection function
+const express = require("express");                  // Import Express framework
+require("dotenv").config();                           // Load environment variables from .env file
+const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import database connection
 const morganLogger = require("./middlewares/loggerService"); // Import the custom Morgan logger
 const { handleError } = require("./utils/handleErrors"); // Import error handling utilities
 
-
-// Create an instance of Express
-const app = express();
-
-// Define the port to listen on, defaulting to 8181
-const PORT = process.env.PORT || 8181;
+const app = express();                                // Initialize Express application
+const PORT = process.env.PORT || 8181;               // Set the port to the specified environment variable or default to 8181
 
 // Connect to the MongoDB database
 connectToMongodb();
 
+// Use the Morgan logger middleware
+app.use(morganLogger);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    // Handle error responses
+    handleError(res, err.status || 500, err.message || "Internal Server Error");
+});
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`); // Log the server start message
+    console.log(`Server is listening on port ${PORT}`);
 });
