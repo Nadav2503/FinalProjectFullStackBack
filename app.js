7003200edba43940c4033985dad14c53022786d8
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 09:26:59 +0300

    apply the modules i need to run the server

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const express = require("express");  // Web framework for Node.js
 require("dotenv").config();           // Load environment variables from .env file
 const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import the database connection function
 
+// Create an instance of Express
+const app = express();
+
+// Define the port to listen on, defaulting to 8181
+const PORT = process.env.PORT || 8181;
+
+// Connect to the MongoDB database
+connectToMongodb();
+
 
