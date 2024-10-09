f7048a4bbff134d185a061360a1146e68fd90d75
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 10:31:24 +0300

    add start and dev to package to run app in dev mode or product mode + correct the import for handleError

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express"); // Import Express framework
 const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import database connection
 const loggerMiddleware = require("./middlewares/loggerService"); // Import Morgan logger
-const { handleError } = require("./utils/handleErrors"); // Import error handler
+const { handleError } = require("./middlewares/errorHandler"); // Import error handler
 const chalk = require("chalk"); // Import Chalk for colored logging
 require("dotenv").config(); // Load environment variables
 
@@ -9,7 +9,7 @@ const app = express(); // Create an Express application
 const PORT = process.env.PORT || 8181; // Set the port
 
 app.use(express.json()); // Enable JSON parsing
-app.use(loggerMiddleware()); // Use the logger middleware
+app.use(loggerMiddleware); // Use the logger middleware
 
 // Centralized error handling middleware
 app.use((err, req, res, next) => {
