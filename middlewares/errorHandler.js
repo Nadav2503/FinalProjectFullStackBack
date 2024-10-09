d98c0c92f1affac60e02ab79ec9b0b910d3a5f6c
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 09:50:23 +0300

    add error message function and chalk

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,5 @@
+const chalk = require("chalk");                   // Import Chalk for colored error logging
+
 // Function to create a validation error
 const createError = (validator, error) => {
     error.message = `${validator} Error: ${error.message}`; // Append validator name to error message
@@ -5,5 +7,11 @@ const createError = (validator, error) => {
     throw new Error(error);                               // Throw a new error
 };
 
-// Export the error handling function
-module.exports = { createError };
+// Function to handle errors and send response
+const handleError = (res, status, message = "") => {
+    console.log(chalk.redBright(message));              // Log the error message in red
+    return res.status(status).send(message);             // Send the error response with status code
+};
+
+// Export the error handling functions
+module.exports = { createError, handleError };
