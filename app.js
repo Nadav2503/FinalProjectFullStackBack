ede5582c9a58e7ec7c389df227d78a74dee86747
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 09:52:45 +0300

    add the import for logger and error in app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express");  // Web framework for Node.js
 require("dotenv").config();           // Load environment variables from .env file
 const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import the database connection function
+const morganLogger = require("./middlewares/loggerService"); // Import the custom Morgan logger
+const { handleError } = require("./utils/handleErrors"); // Import error handling utilities
+
 
 // Create an instance of Express
 const app = express();
