ea3203c22fbedd1d251b0e88c9267500a1745267
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 13:15:05 +0300

    add image schema

---
+++ models/Image.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { URL, DEFAULT_VALIDATION } = require("./defaults");
+
+
+const ImageSchema = new mongoose.Schema({
+    url: URL, // Uses the URL validator defined in your validators
+    alt: DEFAULT_VALIDATION, // Uses the default validation defined in your validators
+});
+
+module.exports = mongoose.model("Image", ImageSchema);
