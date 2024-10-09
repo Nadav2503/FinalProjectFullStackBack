77e14ba26dcd1f3e310695edeecf5de79e30936c
Nadav2503 <nadavivon.re@gmail.com>
2024-10-09 09:26:09 +0300

    import the things i need for start the server

---
+++ app.js
@@ -0,0 +1,6 @@
+// Import required modules
+const express = require("express");  // Web framework for Node.js
+require("dotenv").config();           // Load environment variables from .env file
+const connectToMongodb = require("./DB/mongodb/connectToMongodb"); // Import the database connection function
+
+
