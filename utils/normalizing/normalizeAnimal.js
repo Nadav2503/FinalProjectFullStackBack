6e10941b6d73da6ba9492f4fa2e849c0114c89b7
Nadav2503 <nadavivon.re@gmail.com>
2024-10-10 17:49:37 +0300

    add dedault picture for animal

--- utils/normalizing/normalizeAnimal.js
+++ utils/normalizing/normalizeAnimal.js
@@ -1,8 +1,10 @@
+const defaultAnimalImageUrl = "/images/placeholderAnimalPicture.webp"; // Path to default animal image
+
 const normalizeAnimal = (rawAnimal) => {
     return {
         ...rawAnimal,  // Spread existing properties
         image: {
-            url: rawAnimal.image?.url || "https://default-animal-image-url.com/default.jpg", // Set a default image URL
+            url: rawAnimal.image?.url || defaultAnimalImageUrl, // Use the default animal image if no specific image URL is provided
             alt: rawAnimal.image?.alt || "Default animal image", // Set a default alt text
         },
         healthStatus: rawAnimal.healthStatus || "Unknown", // Set a default health status if not provided
