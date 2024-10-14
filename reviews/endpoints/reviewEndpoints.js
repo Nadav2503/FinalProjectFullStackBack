8d794cd2515f153ab0e44f7de426e05fccc2e073
Nadav2503 <nadavivon.re@gmail.com>
2024-10-14 09:47:52 +0300

    add get all reviews for exhibit endpoint

--- reviews/endpoints/reviewEndpoints.js
+++ reviews/endpoints/reviewEndpoints.js
@@ -3,7 +3,9 @@ const { handleError } = require("../../middlewares/errorHandler");
 const auth = require("../../auth/authService");
 const {
     getReviewsForAnimal,
+    getReviewsForExhibit,
     calculateAverageRatingForAnimal,
+    calculateAverageRatingForExhibit,
 } = require("../crud/reviewCrud");
 const router = express.Router();
 
@@ -12,6 +14,17 @@ router.get("/animal/:animalId", auth, async (req, res) => {
     try {
         const reviews = await getReviewsForAnimal(req.params.animalId); // Fetch reviews
         const averageRating = await calculateAverageRatingForAnimal(req.params.animalId); // Calculate average rating
+        res.status(200).send({ reviews, averageRating }); // Return reviews and average rating
+    } catch (error) {
+        handleError(res, error.status || 500, error.message); // Handle unexpected errors
+    }
+});
+
+// GET Zoo/reviews/exhibit/:exhibitId - Get all reviews for a specific exhibit
+router.get("/exhibit/:exhibitId", auth, async (req, res) => {
+    try {
+        const reviews = await getReviewsForExhibit(req.params.exhibitId); // Fetch reviews
+        const averageRating = await calculateAverageRatingForExhibit(req.params.exhibitId); // Calculate average rating
         res.status(200).send({ reviews, averageRating }); // Return reviews and average rating
     } catch (error) {
         handleError(res, error.status || 500, error.message); // Handle unexpected errors
