0604c8c8bf30ea5b578cba36968b14b8a3d695e1
Nadav2503 <nadavivon.re@gmail.com>
2024-10-14 08:09:23 +0300

    add comments to review model

--- reviews/model/Review.js
+++ reviews/model/Review.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 
+// Define Review model
 const reviewSchema = new mongoose.Schema({
+    // Reference to the Visitor who created the review
     visitorId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Visitor',
         required: true
     },
+    // Optional reference to the associated Exhibit
     exhibitId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Exhibit'
     },
+    // Optional reference to the associated Animal
     animalId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Animal'
     },
-    likes: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Visitor'
-        }
-    ],
+    // Array of Visitor IDs who liked this review
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Visitor'
+    }],
+    // Date when the review was created 
     date: {
         type: Date,
         default: Date.now
     },
+    // Rating given in the review, must be between 1 and 5 
     rating: {
         type: Number,
         min: 1,
@@ -35,4 +40,5 @@ const reviewSchema = new mongoose.Schema({
 // Create the Review model
 const Review = mongoose.model('Review', reviewSchema);
 
+// Export the Review model
 module.exports = Review;
