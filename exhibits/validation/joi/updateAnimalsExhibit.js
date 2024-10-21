e322e22d89a6e419420dd2ce80befbd8068ac31e
Nadav2503 <nadavivon.re@gmail.com>
2024-10-21 18:55:07 +0300

    add xor instead of or since only add or remove can happend each time

--- exhibits/validation/joi/updateAnimalsExhibit.js
+++ exhibits/validation/joi/updateAnimalsExhibit.js
@@ -5,7 +5,7 @@ const exhibitAnimalsUpdateValidate = (animals) => {
     const animalUpdateSchema = Joi.object({
         addAnimals: Joi.array().items(Joi.string()).optional(), // Array of animal IDs to add
         removeAnimals: Joi.array().items(Joi.string()).optional(), // Array of animal IDs to remove
-    }).or('addAnimals', 'removeAnimals'); // At least one of these fields must be present
+    }).xor('addAnimals', 'removeAnimals'); // At least one of these fields must be present
 
     return animalUpdateSchema.validate(animals);
 };
