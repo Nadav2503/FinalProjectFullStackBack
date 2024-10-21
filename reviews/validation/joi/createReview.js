72d070728fc6da5bd20d8ff7d1eb10c78c6db347
Nadav2503 <nadavivon.re@gmail.com>
2024-10-21 20:07:47 +0300

    add xor option in the create review for disable the visitor to write a review for animal and exhibit the same time

--- reviews/validation/joi/createReview.js
+++ reviews/validation/joi/createReview.js
@@ -8,7 +8,7 @@ const createReviewValidate = (review) => {
         animalId: Joi.string().optional(),   // Optional reference to the Animal
         rating: Joi.number().min(1).max(5).required(), // Rating must be between 1 and 5
         comment: Joi.string().trim().min(1).max(256).required(), // Comment is required and must not be empty
-    });
+    }).xor('exhibitId', 'animalId'); // Ensure at least one ID is present
 
     return createReviewSchema.validate(review);
 };
